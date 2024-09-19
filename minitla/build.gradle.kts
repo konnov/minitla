cca3671dfb9cda7d0a0f24073917c816f56a41f0
Igor Konnov <igor@konnov.phd>
2024-09-19 21:53:39 +0200

    make the trivial Main run

--- minitla/build.gradle.kts
+++ minitla/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     id("java")
+    application
 }
 
-group = "com.github.konnov"
-version = "1.0-SNAPSHOT"
+group = "com.github.konnov.minitla"
+version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -12,6 +13,10 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+application {
+    mainClass = "com.github.konnov.minitla.Main"
 }
 
 tasks.test {
